7e84a954ce34b5b9581b62e64615926a7fb74bcb
Jessica Holmes <kirk617@gmail.com>
2018-01-05 11:01:33 -0600

    Adding copy of  ViReMa

--- archive-slurm.sh
+++ archive-slurm.sh
@@ -4,6 +4,7 @@
 #SBATCH --mem=48G
 #SBATCH -J archive
 
+DIR=/home/groups/hpcbio_shared/cbrooke_lab/
 
 if [ -z "$DIR" ]; then
     echo "Must specify a directory"
@@ -12,6 +13,7 @@ fi
 cd $SLURM_SUBMIT_DIR
 
 module load pigz/2.3.4-IGB-gcc-4.9.4
-module load perl/5.24.1
+#module load perl/5.24.1
+module load Perl/5.24.1-IGB-gcc-4.9.4
 
-perl /home/groups/hpcbio/dev/HPCBio-Toolbox/archive-util/prep_for_archive.pl -d $DIR
+perl /home/a-m/jholmes5/bin/perl/prep_for_archive-slurm.pl -d $DIR
