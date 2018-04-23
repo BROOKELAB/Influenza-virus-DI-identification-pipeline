56e868d9a9479acc7786ef1cfb109e0d29cb5ca5
Jessica Holmes <kirk617@gmail.com>
2018-04-23 13:55:42 -0500

    changed to work w/ new script

--- archive-slurm.sh
+++ archive-slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -c 8
+#SBATCH -c 1
 #SBATCH --mem=48G
 #SBATCH -J archive
 
@@ -13,7 +13,6 @@ fi
 cd $SLURM_SUBMIT_DIR
 
 module load pigz/2.3.4-IGB-gcc-4.9.4
-#module load perl/5.24.1
 module load Perl/5.24.1-IGB-gcc-4.9.4
 
-perl /home/a-m/jholmes5/HPCBio-Toolbox/archive-util/prep_for_archive.pl -d $DIR
+perl $DIR/src/Brooke-DARPA/prep_for_archive_brooke.pl -d $DIR
