ccbd6189fd58b6b7e5a1f85aeca9e3984c0332a8
Jessica Holmes <kirk617@gmail.com>
2018-05-08 16:25:33 -0500

    Update archive-slurm.sh

--- archive-utils/archive-slurm.sh
+++ archive-utils/archive-slurm.sh
@@ -14,7 +14,7 @@ fi
 
 cd $SLURM_SUBMIT_DIR
 
-#module load pigz/2.3.4-IGB-gcc-4.9.4
+module load pigz/2.3.4-IGB-gcc-4.9.4
 module load Perl/5.24.1-IGB-gcc-4.9.4
 
 perl /home/groups/hpcbio_shared/cbrooke_lab/Brooke-DARPA/archive-utils/prep_for_archive_brooke.pl -d $DIR
